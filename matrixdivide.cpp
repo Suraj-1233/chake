76671f80edeebaf17bedfa5f43f1e543effc8f16
suraj kannujiya <82014093+Suraj-1233@users.noreply.github.com>
2021-09-20 22:33:27 +0530

    Add files via upload

---
+++ matrixdivide.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+   int matrix3[4][4];
+void divide( int a,int b,int size1)
+  {
+
+      int m[n][n];
+     if(size1==1)
+      m[0][0]=a[0]*b[0];
+      else
+      {
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      }
+            
+        
+
+
+  }
+
+
+
+
+
+int main()
+{ 
+    int size1;
+    cin>>size1;
+    int matrix1[size1][size1];
+     int matrix2[size1][size1];
+      int matrix3[size1][size1];
+
+   for(int i=0;i<size1;i++)
+      {
+      for(int j=0;j<size1;j++)
+               cin>>matrix1[i][j];
+
+      }
+
+      for(int i=0;i<size1;i++)
+      {
+      for(int j=0;j<size1;j++)
+               cin>>matrix2[i][j];
+
+      }
+     
+        
+     
+
+
+
+ 
+
+
+     
+
+
+
+
+
+
+}
